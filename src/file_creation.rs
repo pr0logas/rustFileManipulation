4c3e4b84736e7219bd0928e159ed683a6891234c
Tomas <prologas@protonmail.com>
2020-04-30 19:47:27 +0300

    File creation, open

--- src/file_creation.rs
+++ src/file_creation.rs
@@ -1,16 +1,9 @@
-use std::error::Error;
 use std::fs::File;
 
-enum Result<T, E> {
-    OK(T),
-    Err(E),
-}
-
-pub fn check_file(name: &str) -> Box<String> {
-    let f = File::open(name).expect("No such a file");
-    f
+pub fn check_file(name: &str) {
+    let _f = File::open(name).expect("No file or can't open");
 }
 
 pub fn create_file(name: &str) {
-    let f = File::create(name).expect("File creation failed");
+    let _f = File::create(name).expect("File creation failed");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod file_creation;
 
 fn main() {
     let f_name: &str = "data.txt";
+    file_creation::create_file(f_name);
     file_creation::check_file(f_name);
 }
