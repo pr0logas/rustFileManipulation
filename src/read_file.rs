03916ed415d651c093f8d68ff3acc01948677515
Tomas <prologas@protonmail.com>
2020-04-30 20:24:47 +0300

    Final touches

--- src/read_file.rs
+++ src/read_file.rs
@@ -1,6 +1,9 @@
 use std::io::Read;
 
 pub fn read(fname: &str) -> String {
-    let f = std::fs::File::open(fname).unwrap();
-    f
+    let mut f = std::fs::File::open(fname).unwrap();
+    let mut contents = String::new();
+    let c = f.read_to_string(&mut contents).unwrap();
+    let res = format!("{:?}", c);
+    res
 }
