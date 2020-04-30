f3aa576bc2b5fc30be1417a3ee782200701076a4
Tomas <prologas@protonmail.com>
2020-04-30 20:06:36 +0300

    iterator through i32

--- src/append_data.rs
+++ src/append_data.rs
@@ -1,11 +1,14 @@
 use std::fs::OpenOptions;
+use std::io::Write;
 
-pub fn append_data(fname: &str, count: i32, data: String) -> &str {
-    let mut f = OpenOptions::new()
-        .append(true)
-        .open(fname)
-        .expect("Cannot open file");
-    f.write_all(data.as_bytes()).expect("Write failed");
+pub fn write(fname: &str, count: i32, data: String) -> &str {
+    for i in 0..=count {
+        let mut f = OpenOptions::new()
+            .append(true)
+            .open(fname)
+            .expect("Cannot open file");
+        f.write_all(data.as_bytes()).expect("Write failed");
+    }
     let status: &str = "OK";
     status
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ mod file_creation;
 fn main() {
     let mut data_text: String = String::from("Rust yra super fast and sonic boom right now");
     let f_name: &str = "data.txt";
-    let count: isize = 34;
+    let count: i32 = 34;
     file_creation::create_file(f_name);
     file_creation::check_file(f_name);
 
-    append_data
+    data_text.push_str("\n Forgot something, adding this too;");
+    append_data::write(f_name, count, data_text);
 }
